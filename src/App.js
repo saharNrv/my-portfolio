ac385b8b2acf6db5a224dc874da4adf217908ef7
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-01-13 23:47:23 +0330

    add tailwind and create navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import NavBar from './Components/NavBar/NavBar';
 
 export default function App() {
   return (
     <div>
+      <NavBar/>
       <Header/>
     </div>
   );
